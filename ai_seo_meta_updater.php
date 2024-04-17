34c321d7aa2ee6d3b69da79de181ca7e2e111cd7
Tomas Bance <tomas.bance@gmail.com>
2024-04-17 19:02:40 +0200

    init

---
+++ ai_seo_meta_updater.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: SEO Meta Updater
+Description: AI analizes product title, description, short description, generates 
+    meta and updates fields. 
+Version: 1.0
+Author: Tomas Bancevicius
+*/
+
+
